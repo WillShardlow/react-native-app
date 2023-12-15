a94f96ec36790fd33c9e41558d40625cd9411c80
Will Shardlow <Will.Shardlow@ghyston.com>
2023-12-15 11:59:30 +0000

    add eslint

--- features/profile/Shoes.tsx
+++ features/profile/Shoes.tsx
@@ -1,7 +1,3 @@
 import { Text } from "../../components/Text";
-import { NativeStackScreenProps } from "@react-navigation/native-stack";
-import { ProfileStackParamList } from "./Profile";
 
-type ShoesProps = NativeStackScreenProps<ProfileStackParamList, "Shoes">;
-
-export const Shoes = ({ navigation }: ShoesProps) => <Text>Shoes</Text>;
+export const Shoes = () => <Text>Shoes</Text>;

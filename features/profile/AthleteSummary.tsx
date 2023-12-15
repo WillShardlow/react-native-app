c9aac4eccd14b6debcfd69d96780621e3b069cc4
Will Shardlow <Will.Shardlow@ghyston.com>
2023-12-15 09:52:09 +0000

    move strava query up a level

--- features/profile/AthleteSummary.tsx
+++ features/profile/AthleteSummary.tsx
@@ -1,42 +1,26 @@
-import {
-  AthleteInformationResponse,
-  fetchAthleteInformation,
-} from "../../integrations/strava";
-import { useEffect, useState } from "react";
+import { AthleteInformationResponse } from "../../integrations/strava";
 import { Button, Image, StyleSheet, View } from "react-native";
 import { Text } from "../../components/Text";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import { ProfileStackParamList } from "./Profile";
 
-type AthleteSummaryProps = NativeStackScreenProps<
+export type AthleteSummaryProps = NativeStackScreenProps<
   ProfileStackParamList,
   "AthleteSummary"
->;
-
-export const AthleteSummary = ({ navigation }: AthleteSummaryProps) => {
-  const [athleteInformation, setAthleteInformation] =
-    useState<AthleteInformationResponse>();
-
-  useEffect(() => {
-    const getAthleteInformation = async () => {
-      const athleteInformation = await fetchAthleteInformation();
-      setAthleteInformation(athleteInformation);
-    };
-
-    getAthleteInformation();
-  }, []);
-
-  return (
-    <View style={styles.container}>
-      <Image
-        source={{ uri: athleteInformation?.profile }}
-        style={styles.image}
-      />
-      <Text>{`${athleteInformation?.firstname} ${athleteInformation?.lastname}`}</Text>
-      <Button title="Shoes" onPress={() => navigation.push("Shoes")} />
-    </View>
-  );
+> & {
+  athleteInformation: AthleteInformationResponse;
 };
+
+export const AthleteSummary = ({
+  navigation,
+  athleteInformation,
+}: AthleteSummaryProps) => (
+  <View style={styles.container}>
+    <Image source={{ uri: athleteInformation.profile }} style={styles.image} />
+    <Text>{`${athleteInformation.firstname} ${athleteInformation.lastname}`}</Text>
+    <Button title="Shoes" onPress={() => navigation.push("Shoes")} />
+  </View>
+);
 
 const imageSizeInPixels = 200;
 
--- features/profile/Profile.tsx
+++ features/profile/Profile.tsx
@@ -1,6 +1,12 @@
 import { AthleteSummary } from "./AthleteSummary";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { Shoes } from "./Shoes";
+import { useEffect, useState } from "react";
+import {
+  AthleteInformationResponse,
+  fetchAthleteInformation,
+} from "../../integrations/strava";
+import { Text } from "../../components/Text";
 
 export type ProfileStackParamList = {
   AthleteSummary: undefined;
@@ -9,19 +15,46 @@ export type ProfileStackParamList = {
 
 const Stack = createNativeStackNavigator<ProfileStackParamList>();
 
-export const Profile = () => (
-  <>
-    <Stack.Navigator>
-      <Stack.Screen
-        name="AthleteSummary"
-        component={AthleteSummary}
-        options={{ title: "Summary" }}
-      />
-      <Stack.Screen
-        name="Shoes"
-        component={Shoes}
-        options={{ title: "Shoes" }}
-      />
-    </Stack.Navigator>
-  </>
-);
+export const Profile = () => {
+  const [athleteInformation, setAthleteInformation] =
+    useState<AthleteInformationResponse>();
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+    const getAthleteInformation = async () => {
+      await fetchAthleteInformation().then((athleteInformation) => {
+        setAthleteInformation(athleteInformation);
+        setIsLoading(false);
+      });
+    };
+
+    getAthleteInformation();
+  }, []);
+
+  return (
+    <>
+      {isLoading ? (
+        <Text>Loading...</Text>
+      ) : athleteInformation ? (
+        <Stack.Navigator>
+          <Stack.Screen name="AthleteSummary" options={{ title: "Summary" }}>
+            {(props) => (
+              <AthleteSummary
+                athleteInformation={athleteInformation}
+                {...props}
+              />
+            )}
+          </Stack.Screen>
+          <Stack.Screen
+            name="Shoes"
+            component={Shoes}
+            options={{ title: "Shoes" }}
+          />
+        </Stack.Navigator>
+      ) : (
+        <Text>No athlete data</Text>
+      )}
+    </>
+  );
+};
